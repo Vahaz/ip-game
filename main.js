dee792f5a5b6fa9406f3afafbced4f59a1841929
Vahaz <47829911+Vahaz@users.noreply.github.com>
2026-02-14 17:13:09 +0100

    Update 3.0 - Reworked site aspect - Changed site colors (dark theme > bright theme) - Reworked main.js functions - Removed Binary addresses - Added head meta - Added favicon / thumbnail - Removed .svg - Changed buttons / Removed buttons (Need to refresh the page to restart) - Added a LICENCE - Added Credits

--- main.js
+++ main.js
@@ -1,201 +1,97 @@
-const inputs = document.querySelectorAll('.input');
-const displayIP = document.getElementById("ip");
-const displayCIDR = document.getElementById("cidr");
+const inputs = document.querySelectorAll("input");
+const btns = document.querySelectorAll(".reveal")
+const ip = document.getElementById("ip");
 
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 class IPv4 {
-    constructor(ip, cidr) {
-        this.ip = ip != null ? ip : [rand(10, 239), rand(1, 254), rand(1, 254), rand(1, 254)];
-        this.cidr = cidr != null ? cidr : rand(8, 30);
+    constructor() {
+        this.ip = [rand(10, 239), rand(1, 254), rand(1, 254), rand(1, 254)];
+        this.cidr = rand(8, 30);
     }
-
     get mask() {
         let bits = this.cidr;
         return Array.from({ length: 4 }, () => {
-            const value = bits >= 8 ? 255 : bits <= 0 ? 0 : 256 - 2 ** (8 - bits);
+            const value = bits >= 8 ? 255 : bits <= 0 ? 0 : 256 - Math.pow(2, 8 - bits);
             bits -= 8;
             return value;
         });
     }
-
-    get network() {
-        return this.ip.map((o, i) => o & this.mask[i]);
+    get network() { return this.ip.map((o, i) => o & this.mask[i]); }
+    get broadcast() { return this.ip.map((o, i) => o | (255 - this.mask[i])); }
+    getString(type) {
+        if (type === "mask") return this.mask.join(".");
+        if (type === "network") return this.network.join(".");
+        if (type === "broadcast") return this.broadcast.join(".");
+        return "";
     }
-
-    get broadcast() {
-        return this.ip.map((o, i) => o | (255 - this.mask[i]));
+    getData(type) {
+        if (type === "mask") return this.mask;
+        if (type === "network") return this.network;
+        if (type === "broadcast") return this.broadcast;
+        return [0, 0, 0, 0];
     }
-
-    toBinary(adr) {
-        return adr.map(o => ("00000000" + o.toString(2)).slice(-8));
-    }
-
-    display() {
-        displayIP.innerText = this.ip.join('.');
-        displayCIDR.innerText = `/${this.cidr}`;
-    }
+    display() { ip.innerText = `${this.ip.join('.')}/${this.cidr}`; }
 }
 
-function reset() {
-    ipv4 = new IPv4();
-    ipv4.display();
-    inputs.forEach((input) => {
-        input.value = '';
-        input.readOnly = false;
-        input.classList.remove("wrong");
-        input.classList.remove("right");
-    })
-}
+let currentIP = new IPv4();
+currentIP.display();
 
-function empty() {
-    inputs.forEach((input) => {
-        input.value = '';
-        input.readOnly = false;
-        input.classList.remove("wrong");
-        input.classList.remove("right");
-    })
-}
+inputs.forEach((input) => {
+    input.value = "";
+    input.addEventListener("input", (e) => update(e, input));
+});
+btns.forEach((btn) => { btn.addEventListener("click", (e) => reveal(btn)); });
 
-let ipv4 = new IPv4();
-ipv4.display();
+function update(event, input) {
+    const type = input.getAttribute("id");
+    let value = input.value;
+    value = value.replace(/[^0-9.]/g, "");
+    const octets = value.split(".");
 
-inputs.forEach((input, i) => {
-    input.addEventListener("input", () => update(input, i));
-})
+    if (octets.length > 0) {
+        const index = octets.length - 1;
+        let oct = octets[index];
 
-function update(input, index) {
-    switch (true) {
-        case (index <= 3):
-            if (ipv4.mask[index] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        case (index > 3 && index <= 7):
-            if (ipv4.toBinary(ipv4.mask)[index-4] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        case (index > 7 && index <= 11):
-            if (ipv4.network[index-8] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        case (index > 11 && index <= 15):
-            if (ipv4.toBinary(ipv4.network)[index-12] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        case (index > 15 && index <= 19):
-            if (ipv4.broadcast[index-16] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        case (index > 19 && index <= 23):
-            if (ipv4.toBinary(ipv4.broadcast)[index-20] == input.value) {
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                nextInput(index);
-                input.readOnly = true;
-            } else {
-                input.classList.remove("right");
-                input.classList.add("wrong");
-            }
-            break;
-        default:
-            input.classList.remove("right");
-            input.classList.add("wrong");
-            break;
-    }
-
-    if (input.value.length >= input.max) {
-        input.value = input.value.slice(0, input.max);
-    }
-}
-
-function nextInput(index) {
-    if (index + 1 == 24) {
-        inputs[index].blur();
-    } else {
-        inputs[index + 1].focus();
-    }
-}
-
-function reveal(index) {
-    for (let i = 0; i < 4; i++) {
-        let input = inputs[index+i];
-        let idx = index+i
-        switch (true) {
-            case (idx <= 3):
-                input.value = ipv4.mask[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            case (idx > 3 && idx <= 7):
-                input.value = ipv4.toBinary(ipv4.mask)[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            case (idx > 7 && idx <= 11):
-                input.value = ipv4.network[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            case (idx > 11 && idx <= 15):
-                input.value = ipv4.toBinary(ipv4.network)[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            case (idx > 15 && idx <= 19):
-                input.value = ipv4.broadcast[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            case (idx > 19 && idx <= 23):
-                input.value = ipv4.toBinary(ipv4.broadcast)[i]
-                input.classList.remove("wrong");
-                input.classList.add("right");
-                input.readOnly = true;
-                break;
-            default:
-                break;
+        if (parseInt(oct) > 255) {
+            oct = 255;
+            octets[index] = oct;
+            value = octets.join(".");
         }
+
+        if (event.inputType != "deleteContentBackward" && octets.length < 4) {
+            const dataArray = currentIP.getData(type);
+            const data = dataArray[index];
+            const isFull = oct.length === 3;
+            const isMatch = parseInt(oct) === data;
+            if (isFull || isMatch) value += ".";
+        }
+    }
+    input.value = value;
+    validate(input);
+}
+
+function reveal(button) {
+    const target = button.dataset.target;
+    const input = document.getElementById(target);
+    if (input) {
+        input.value = currentIP.getString(target);
+        validate(input);
+    }
+}
+
+function validate(input) {
+    const type = input.getAttribute("id");
+    const adr = currentIP.getString(type);
+    input.classList.remove("right", "wrong");
+    input.disabled = false;
+
+    if (input.value === adr) {
+        input.classList.add("right");
+        input.disabled = true
+    } else if (input.value.length > 0) {
+        input.classList.add("wrong");
     }
 }
